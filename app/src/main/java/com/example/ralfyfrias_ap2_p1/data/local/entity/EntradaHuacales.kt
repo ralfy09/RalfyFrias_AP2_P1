5203272ae411ad42453450f452b2a08024ede28b
ralfy <ralfy@RALFY>
2025-10-01 22:12:44 -0300

    fin

---
+++ app/src/main/java/com/example/ralfyfrias_ap2_p1/data/local/entity/EntradaHuacales.kt
@@ -0,0 +1,9 @@
+package com.example.ralfyfrias_ap2_p1.data.local.entity
+
+data class EntradaHuacales(
+    val idEntrada: Int,
+    val fecha: String,
+    val nombreCliente: String,
+    val cantidad: Int,
+    val precio: Double
+)
