7edd4fd83ca68df62060a19146e49facd8d299c4
Ralfy <ralfyfrias@gmail.com>
2025-10-02 02:26:23 -0400

    Creacion de RalfyFrias_AP2_P1App.kt

---
+++ app/src/main/java/com/example/ralfyfrias_ap2_p1/RalfyFrias_AP2_P1App.kt
@@ -0,0 +1,8 @@
+package com.example.ralfyfrias_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RalfyFrias_AP2_P1App : Application() {
+}
