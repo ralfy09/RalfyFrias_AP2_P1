d52df556a0f5de80b6404d60f274c76dba2e54de
Ralfy <ralfyfrias@gmail.com>
2025-10-02 02:26:46 -0400

    Setup Navigation and ViewModel

--- app/src/main/java/com/example/ralfyfrias_ap2_p1/MainActivity.kt
+++ app/src/main/java/com/example/ralfyfrias_ap2_p1/MainActivity.kt
@@ -11,37 +11,48 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.ralfyfrias_ap2_p1.data.local.database.AppDatabase
+import com.example.ralfyfrias_ap2_p1.data.repository.EntradaHuacalesRepositoryImpl
+import com.example.ralfyfrias_ap2_p1.domain.usecase.CountEntradasUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.DeleteEntradaUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.FilterEntradasUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.GetEntradasUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.InsertEntradaUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.TotalEntradasUseCase
+import com.example.ralfyfrias_ap2_p1.domain.usecase.UpdateEntradaUseCase
+import com.example.ralfyfrias_ap2_p1.presentation.list.EntradaHuacalesViewModel
+import com.example.ralfyfrias_ap2_p1.presentation.list.EntradaHuacalesViewModelFactory
 import com.example.ralfyfrias_ap2_p1.ui.theme.RalfyFrias_AP2_P1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            RalfyFrias_AP2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java,
+                "app_db"
+            ).build()
+
+            val repository = EntradaHuacalesRepositoryImpl(db.entradaHuacalesDao())
+
+            val factory = EntradaHuacalesViewModelFactory(
+                InsertEntradaUseCase(repository),
+                UpdateEntradaUseCase(repository),
+                DeleteEntradaUseCase(repository),
+                GetEntradasUseCase(repository),
+                FilterEntradasUseCase(repository),
+                CountEntradasUseCase(repository),
+                TotalEntradasUseCase(repository)
+            )
+
+            val navController = rememberNavController()
+            val viewModel: EntradaHuacalesViewModel = viewModel(factory = factory)
+
+            MyApp(navController, viewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RalfyFrias_AP2_P1Theme {
-        Greeting("Android")
     }
 }
