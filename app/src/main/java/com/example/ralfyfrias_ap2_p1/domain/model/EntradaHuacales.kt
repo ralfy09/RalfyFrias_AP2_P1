b62cb5162ed6caa2987169f7dcaf94d026cd155c
Ralfy <ralfyfrias@gmail.com>
2025-10-02 02:19:03 -0400

    creacion de EntradaHuacales.kt

--- app/src/main/java/com/example/ralfyfrias_ap2_p1/domain/model/EntradaHuacales.kt
+++ app/src/main/java/com/example/ralfyfrias_ap2_p1/domain/model/EntradaHuacales.kt
@@ -1,7 +1,7 @@
-package com.example.ralfyfrias_ap2_p1.data.local.entity
+package com.example.ralfyfrias_ap2_p1.domain.model
 
 data class EntradaHuacales(
-    val idEntrada: Int,
+    val idEntrada: Int = 0,
     val fecha: String,
     val nombreCliente: String,
     val cantidad: Int,
